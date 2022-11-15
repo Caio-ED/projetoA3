f5f340419d0737f48438abe4f5541bb867563fe1
Jhonatan Luan dos Santos <jhonatanluansantos@gmail.com>
2022-11-15 14:21:52 -0300

    Adicionando responsividade

--- Front/src/app/shared/header/header.component.ts
+++ Front/src/app/shared/header/header.component.ts
@@ -10,6 +10,23 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const header = document.querySelector('.header') as HTMLElement | null;
+    const nav = document.querySelector('.header__nav') as HTMLElement | null;
+    const btnMobile = document.querySelector('.header__nav__btnMobile') as HTMLElement | null;
+    const menuMobile = document.querySelector('.header__nav__menu') as HTMLElement | null;
+
+
+    let sizeHeight = header.offsetHeight;
+    menuMobile.style.top = `${sizeHeight}px`;
+
+
+    btnMobile.addEventListener('click', () => {
+      if (nav.classList.contains('active')) {
+        nav.classList.remove('active')
+      } else {
+        nav.classList.add('active')
+      }
+    })
   }
 
 }
