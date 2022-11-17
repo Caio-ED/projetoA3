817a3bbd937ebf8ef51fb52736c3f11b963ded30
GucciNG <gustavo.nascimento07@outlook.com>
2022-11-17 20:04:11 -0300

    feat: API do CEP

--- Front/src/app/pacientes/cadastro-pacientes/cadastro-pacientes.component.ts
+++ Front/src/app/pacientes/cadastro-pacientes/cadastro-pacientes.component.ts
@@ -36,7 +36,6 @@ export class CadastroPacientesComponent implements OnInit {
     })
   }
 
-
   constructor(
     private httpClient: HttpClient,
     private rota: Router) { 
@@ -45,6 +44,23 @@ export class CadastroPacientesComponent implements OnInit {
     }
 
   ngOnInit(): void {
+    const fillForm = (address) =>{
+      (document.querySelector('#rua') as HTMLInputElement).value = address.logradouro;
+      (document.querySelector('#complemento') as HTMLInputElement).value= address.complemento;
+      (document.querySelector('#bairro') as HTMLInputElement).value = address.bairro;
+      (document.querySelector('#cidade') as HTMLInputElement).value = address.localidade;
+      (document.querySelector('#estado') as HTMLInputElement).value= address.uf;
+    }
+
+    const searchCep = async() => {
+      const cep = (document.querySelector('#cep') as HTMLInputElement).value;
+      const url = `http://viacep.com.br/ws/${cep}/json/`;
+      const dados = await fetch(url);
+      const address = await dados.json()
+      fillForm(address)
+    };
+
+    document.querySelector('#cep').addEventListener('focusout', searchCep)
     
   }
 
